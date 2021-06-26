f4b278e522153fe59e0b4bfcff43ce8c556b924a
charlie <charm@kth.se>
2021-06-26 14:27:55 +0200

    Add classes for game logic and moves & skeleton of display class

---
+++ gamelogic.py
@@ -0,0 +1,126 @@
+import sys, pygame as pg, random
+from gridmoves import Moves
+pg.init()
+
+class GameLogic:
+
+    #allows us to use the moving functionality
+    moves = Moves()
+
+    def __init__(self, grid_size, target_score):
+        self.GRID_SIZE = grid_size
+        self.TARGET_SCORE = target_score
+        self.game_over = False
+        self.game_won = False
+        self.game_grid = [[0 for x in range(self.GRID_SIZE)] for y in range(self.GRID_SIZE)]
+        self.empty_cells = [(x, y) for x in range(self.GRID_SIZE) for y in range(self.GRID_SIZE)]
+
+
+    #GRID_SIZE = 4           # expand this to be optional later and add 3x3 and 5x5
+    #TARGET_SCORE = 2048
+    # initialize N x N game grid
+    # keep track of empty cells for spawning new blocks; initially all cells empty
+
+    """
+    Checks if the target score has been attained. If yes, the game is won.
+    """
+    def check_win(self):
+        for row in self.game_grid:
+            if self.TARGET_SCORE in row:
+                self.game_won = True
+                print("You win! :D  You can keep playing though!")
+                break 
+
+    """
+    This should only be run when the board is FULL, otherwise there are always
+    available moves.
+    Checks if every game state resulting from a future move is the same.
+    If that is the case, the game is lost.
+    If it's possible to make a move that changes the game state, we can keep going.
+    """
+    def check_loss(self):
+        if  GameLogic.moves.move_left(self.game_grid) == self.game_grid and \
+            GameLogic.moves.move_right(self.game_grid) == self.game_grid and \
+            GameLogic.moves.move_up(self.game_grid) == self.game_grid and \
+            GameLogic.moves.move_down(self.game_grid) == self.game_grid:
+                self.game_over = True
+                print("You lose!")
+                sys.exit()
+
+    """
+    Creates a new tile in an empty cell after a legal move.
+    """
+    def spawn_tile(self):
+        if random.random() > 0.9:
+            #10% probability to spawn a 4
+            val = 4
+        else:   
+            val = 2
+
+        # retrieve a random cell from the list of empty cells, and put the new tile there
+        row, col = self.empty_cells[random.randrange(len(self.empty_cells))]  #only reading so we don't need to declare global
+        self.game_grid[row][col] = val
+
+    def gameloop(self):
+        spawn_on_next_turn = True
+        print(self.game_grid)
+
+        while True:
+
+            if spawn_on_next_turn:
+                self.spawn_tile()
+
+            # draw here
+            for row in self.game_grid:
+                print(row)  #placeholder
+
+            # listen for move.
+            # update the game state to match the move.
+            """for event in pg.event.get():
+                if event.type == pg.KEYDOWN:
+                    if event.key == K_LEFT:
+                        game_grid = move_left(game_grid)
+                    elif event.key == K_RIGHT:
+                        game_grid = move_left(game_grid)
+                    elif event.key == K_UP:
+                        game_grid = move_up(game_grid)
+                    elif event.key == K_DOWN:
+                        game_grid = move_down(game_grid)
+                    elif event.key == K_q:  #quit
+                        sys.exit()
+            """
+
+            # command line version
+            move = input()
+            spawn_on_next_turn = True
+            if move == "left" and self.game_grid != GameLogic.moves.move_left(self.game_grid):
+                self.game_grid = GameLogic.moves.move_left(self.game_grid)
+            elif move == "right" and self.game_grid != GameLogic.moves.move_right(self.game_grid):
+                self.game_grid = GameLogic.moves.move_right(self.game_grid)
+            elif move == "up" and self.game_grid != GameLogic.moves.move_up(self.game_grid):
+                self.game_grid = GameLogic.moves.move_up(self.game_grid)
+            elif move == "down" and self.game_grid != GameLogic.moves.move_down(self.game_grid):
+                self.game_grid = GameLogic.moves.move_down(self.game_grid)
+            elif move == "quit":  #quit
+                sys.exit()
+            else:
+                spawn_on_next_turn = False
+                print("YOU FUCKED UP URURHGURHGUHG")
+
+            # update the list of empty cells
+            # this feels absolutely horrible, idk if it's better to just clear it
+            for row in range(self.GRID_SIZE):
+                for col in range(self.GRID_SIZE):
+                    if self.game_grid[row][col] == 0 and (row, col) not in self.empty_cells:
+                        self.empty_cells.append((row, col))
+                    elif self.game_grid[row][col] != 0 and (row, col) in self.empty_cells:
+                        self.empty_cells.remove((row, col))
+
+            if(len(self.empty_cells) == 0): 
+                self.check_loss()
+
+            self.check_win()
+
+
+game = GameLogic(4, 2048)
+game.gameloop()
---
+++ gridmoves.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+
+class Moves:
+
+    ### MOVEMENT FUNCTIONS
+    # Work by matrix manipulation
+    # Makes copies of the game state in order to test moves -- could be optimized?
+    # Although these are arrays of 16 elements and probably AT ABSOLUTE MOST 100.
+
+    def move_left(self, grid):
+        matrix = np.asarray(grid)
+        for i in range(len(matrix)):
+            matrix[i] = self.compress_list_and_pad(matrix[i], "left")
+            matrix[i] = self.merge_left(matrix[i])
+        return matrix.tolist()
+
+    def move_right(self, grid):
+        matrix = np.asarray(grid)
+        for i in range(len(matrix)):
+            matrix[i] = self.compress_list_and_pad(matrix[i], "right")
+            matrix[i] = self.merge_right(matrix[i])
+        return matrix.tolist()
+
+    def move_up(self, grid):
+        matrix = np.transpose(self.move_left(np.transpose(grid)))
+        return matrix.tolist()        # this syntax is bc numpy works with optimized arrays and not regular lists
+
+    def move_down(self, grid):
+        matrix = np.transpose(self.move_right(np.transpose(grid)))
+        return matrix.tolist()
+
+
+    ### MOVEMENT HELPER FUNCTIONS
+    # Compressing elements in lists
+
+    """
+    Takes a list as an argument.
+    Removes all Nones and returns a list of the remaining elements.
+    Order is preserved.
+
+    This creates a new list, it's not in place. Could be a problem?
+    """
+    def compress_list(self, lst):
+        shifted = []
+        for elem in lst:
+            if elem != 0:
+                shifted.append(elem)
+
+        return shifted
+
+    """
+    Takes a list, compresses its elements, and preserves the length by padding the
+    list with 0. The elements are placed on the side of the list indicated by
+    the argument shift_elems_to.
+
+    Args:
+        - lst: the list to be compressed
+        - shift_elems_to: options 'left' or 'right' to indicate to which side the
+        elements go, i.e. the padding goes on the other side.
+
+    Returns a new list with the elements of the original list compressed and the
+    extra space padded.
+    """
+    def compress_list_and_pad(self, lst, shift_elems_to):
+        original_length = len(lst)
+        lst = self.compress_list(lst)
+        try:
+            if len(lst) < original_length:
+                if shift_elems_to.lower() == "left":
+                    lst.extend([0]*(original_length - len(lst)))
+                elif shift_elems_to.lower() == "right":
+                    lst[:0] = [0]*(original_length - len(lst))
+        except ValueError:
+            print("You tried to shift the list in some illegal direction")
+
+        return lst
+
+    """
+    Combines tiles with left adjacency taking precedence.
+    Adjacent identical tiles are merged into one tile with twice the value.
+    Returns a list with spaces between merged tiles removed, left-shifted.
+    """
+    def merge_left(self, lst):
+        if len(lst) < 2: return lst 
+
+        # Starting from the left side, checks every 2 adjacent elements for
+        # equality; if they are equal, they are combined.
+        # Bound of len-1 is EXCLUSIVE, so i+1 is at most len-1.
+        for i in range(0, len(lst)-1):
+            if lst[i] == lst[i+1] and lst[i] != 0:
+                lst[i] = 2*lst[i]
+                lst[i+1] = 0
+
+        # get rid of remaining nones
+        return self.compress_list_and_pad(lst, "left")
+
+    """
+    Combines tiles with right adjacency taking precedence.
+    Adjacent identical tiles are merged into one tile with twice the value.
+    Returns a list with spaces between merged tiles removed, right-shifted.
+    """
+    def merge_right(self, lst):
+        if len(lst) < 2: return lst 
+
+        # Starting from the right side, checks every 2 adjacent elements for
+        # equality; if they are equal, they are combined
+        # Bound of 0 is EXCLUSIVE, so i-1 is 0 at its smallest.
+        for i in range(len(lst)-1, 0, -1):
+            if lst[i] == lst[i-1] and lst[i] != 0:
+                lst[i] = 2*lst[i]
+                lst[i-1] = 0
+
+        # get rid of remaining nones
+        return self.compress_list_and_pad(lst, "right")
